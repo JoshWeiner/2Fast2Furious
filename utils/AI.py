f4bfef9a99eff2cd3c9e5571c968839ae260af66
Josh Weiner <joshuaweinernyc@gmail.com>
2018-06-17 01:12:34 -0400

    made AI mostly functional

--- utils/AI.py
+++ utils/AI.py
@@ -7,7 +7,9 @@ import cgi
 
 addTo = False
 listedOptions = False
+addResp = False
 storeStr = ""
+storeFilepath = ""
 
 def printSys(inputTxt):
 	if listedOptions == False:
@@ -22,25 +24,31 @@ def compTxt(inputTxt):
 	global addTo
 	inputs = os.listdir("./static/Inputs")
 	for file in inputs:
-		readInputFile = open("./static/Inputs/" + file, "r").readlines()
+		filepath = "./static/Inputs/" + file
+		readInputFile = open(filepath, "r")
+		r = readInputFile.readlines()
 		newStr = inputTxt.split(" ")
-		for word in newStr:
-			if (len(word) > 4) and word in readInputFile:
-				print("BOOL")
-				if file != "farewell":
-					readOutputFile = open("./static/Outputs/" + os.path.basename(file), "r").read().split("\n")
-					return(random.choice(readOutputFile))
-				else:
-					readOutputFile = open("./static/Outputs/" + os.path.basename(file), "r").read().split("\n")
-					return random.choice(readOutputFile)
-				sys.exit()
+		for line in r:
+			for word in newStr:
+				if word in line and len(word) > 3:
+					categs = os.listdir("./static/Outputs")
+					if file in categs:
+						if file != "farewell":
+							readOutputFile = open("./static/Outputs/" + file, "r").read().split("\n")
+							return(random.choice(readOutputFile))
+						else:
+							readOutputFile = open("./static/Outputs/" + file, "r").read().split("\n")
+							return random.choice(readOutputFile)
+						sys.exit()
 	addTo = True;
 	return defProtocol(inputTxt)
 
 def defProtocol(inputTxt):
 	global addTo
 	global listedOptions
+	global addResp
 	global storeStr
+	global storeFilepath
 	retStr = ""
 	if addTo == True:
 		if listedOptions == False:
@@ -54,12 +62,21 @@ def defProtocol(inputTxt):
 				categNames.append(os.path.basename(file))
 			listedOptions = True
 		else:
-			inputFile = open("./static/Inputs/" + inputTxt , "a+")
-			inputFile.write("\n" + storeStr)
-			inputFile.close()
-	 		retStr += "Thanks for improving me!"
-			listedOptions = False
-			addTo = False
+			if addResp == True:
+				outputFile = open("./static/Outputs/" + storeFilepath , "a+")
+				outputFile.write("\n" + inputTxt)
+				outputFile.close()
+				retStr += "Thank you for the input!"
+				listedOptions = False
+				addTo = False
+				addResp = False
+			else:
+				storeFilepath = inputTxt
+				inputFile = open("./static/Inputs/" + storeFilepath , "a+")
+				inputFile.write("\n" + storeStr)
+				inputFile.close()
+		 		retStr += "Please let me know how I should respond to that."
+				addResp = True
 		return retStr
 
 #printSys()
