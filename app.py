23cf6c95f9ddaaa2ffff9f6a1cb063f9d539fc7c
JoshWeiner <joshuaweinernyc@gmail.com>
2018-06-17 04:44:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,17 @@ from utils import AI as ai
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["POST", "GET"])
 def home():
     return render_template("index.html")
 
 @app.route("/get_js", methods = ["POST", "GET"])
 def get_js():
-    if request.method == "POST":
-        data = request.form['rawText']
-        newData = ai.printSys(data)
-        return newData
-    return render_template('index.html')
+    data = request.form['rawText']
+    newData = ai.compTxt(data)
+    return newData
 
-@app.route("/get_bot")
+@app.route("/get_bot", methods = ["POST", "GET"])
 def get_bot_response():
     botText = render_v2py("AI.py", comp)
     return str(render_template("index.html", botText = botText))
