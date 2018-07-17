6a40c184f8db76233f839e840450a8615be505bf
Josh Weiner <joshuaweinernyc@gmail.com>
2018-07-17 00:37:39 -0400

    fixing

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask, render_template, request
 from utils import AI as ai
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["POST", "GET"])
+@app.route("/index", methods = ["POST", "GET"])
 def home():
     return render_template("index.html")
 
@@ -22,3 +25,5 @@ def get_bot_response():
 
 if __name__ == "__main__":
     app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
